f8bfc6b5d0055c5aa9702d3a0ec1c50045e449ef
DanielBeiste <Daniel@10.2.7.81>
2017-03-07 18:35:32 +0100

    prueba1

---
+++ conexionrepositorio/src/conexionrepositorio/clase1.java
@@ -0,0 +1,14 @@
+package conexionrepositorio;
+
+public class clase1 {
+
+	public clase1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
